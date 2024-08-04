88ea663d7bf59ee8b3d9b1997d733b026988d7ea
mehregan <mehregank_9819@hotmail.com>
2024-08-05 01:45:54 +0330

    Change: data structure implementations were switched to structs from classes.
    This is due to the fact that the app is based on call by references and heap allocations which are the basis of linked lists. As such, after considering a multitude of  strategies including classes, private members etc., it was decided to use aggregates instead. What's the point of making a pointer member private? We'll then need getters and setters but those won't provide the protection that getters and setters normally provide.

--- libqeh/include/queued_event_handler.hpp
+++ libqeh/include/queued_event_handler.hpp
@@ -1,24 +1,20 @@
 #include <string>
-#include <string_view>
 
 namespace qeh {
 
-class Event {
- private:
+struct Event {
   std::string m_message{};
   Event* m_next{nullptr};
 };
 
-class Queue {
- private:
+struct Queue {
   std::string m_name{};
   Event* m_first_event{nullptr};
   Event* m_last_event{nullptr};
   Queue* m_next{nullptr};
 };
 
-class QueueList {
- private:
+struct QueueList {
   Queue* m_first{nullptr};
   Queue* m_last{nullptr};
 };
