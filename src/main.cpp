1011402ac63f643b897ebbc5b5f9a7913abeab1f
mehregan <mehregank_9819@hotmail.com>
2024-08-05 04:05:57 +0330

    Add: basic input and processing example to main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
 #include <cstdlib>
 
-int main() { return EXIT_SUCCESS; }
+#include "../libqeh/include/queued_event_handler.hpp"
+
+int main() {
+  const char* event_queue_pairs[] = {
+      "q1\0hi there!",
+      "q2\0hello!",
+      "q3\0ok!",
+      "q2\0no!",
+      "q1\0yes!",
+      "q1\0alright!",
+      "gooood\0this is a longer message.",
+      nullptr,
+  };
+
+  qeh::QueueList* queue_list{new qeh::QueueList{}};
+  abort_if_nullptr(queue_list, "queue list alloc failed. Aborting...");
+
+  std::cout << process_event_queue_pairs(event_queue_pairs, queue_list)
+            << std::endl;
+
+  return EXIT_SUCCESS;
+}
