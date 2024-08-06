1abf3c253ccfe153b6d37d6cffed1ed3ffae8bb6
mehregan <mehregank_9819@hotmail.com>
2024-08-06 20:17:13 +0330

    Add: build config for libqeh test.

--- tests/libqeh/test_process_event_queue_pairs.cpp
+++ tests/libqeh/test_process_event_queue_pairs.cpp
@@ -2,7 +2,8 @@
 
 #include <cstdint>
 
-#include "libqeh/queued_event_handler.hpp"
+#include "queued_event_handler.hpp"
+
 TEST(test_process_event_queue_pairs, test_empty_input) {
   qeh::QueueList queue_list{};
   const char* event_queue_pairs[] = {nullptr, nullptr};
@@ -13,8 +14,8 @@ TEST(test_process_event_queue_pairs, test_empty_input) {
 
 TEST(test_process_event_queue_pairs, test_2_inputs) {
   qeh::QueueList queue_list{};
-  const char* event_queue_pairs[] = {"q1\0this is q1.",
-                                     "q2\0this is q2." nullptr};
+  const char* event_queue_pairs[] = {"q1\0this is q1.", "q2\0this is q2.",
+                                     nullptr};
   int32_t expected{2};
   EXPECT_EQ(expected,
             process_event_queue_pairs(event_queue_pairs, &queue_list));
