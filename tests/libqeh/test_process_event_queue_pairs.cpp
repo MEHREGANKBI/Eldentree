26f0e69c93110d63c5ecb9120acf21abf43ea1ca
mehregan <mehregank_9819@hotmail.com>
2024-08-06 19:43:13 +0330

    Add: test cases for libqeh event queue pair input processor.

---
+++ tests/libqeh/test_process_event_queue_pairs.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+#include <cstdint>
+
+#include "libqeh/queued_event_handler.hpp"
+TEST(test_process_event_queue_pairs, test_empty_input) {
+  qeh::QueueList queue_list{};
+  const char* event_queue_pairs[] = {nullptr, nullptr};
+  int32_t expected{0};
+  EXPECT_EQ(expected,
+            process_event_queue_pairs(event_queue_pairs, &queue_list));
+}
+
+TEST(test_process_event_queue_pairs, test_2_inputs) {
+  qeh::QueueList queue_list{};
+  const char* event_queue_pairs[] = {"q1\0this is q1.",
+                                     "q2\0this is q2." nullptr};
+  int32_t expected{2};
+  EXPECT_EQ(expected,
+            process_event_queue_pairs(event_queue_pairs, &queue_list));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
